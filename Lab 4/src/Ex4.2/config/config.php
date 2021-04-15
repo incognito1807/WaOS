6e5920088d9e664f6be149a17615b924151565d0
incognito1807 <tusupermanvodoi@gmail.com>
2021-04-15 20:20:00 +0700

    fix Ex4.2 Lab 4

--- Lab 4/src/Ex4.2/config/config.php
+++ Lab 4/src/Ex4.2/config/config.php
@@ -10,7 +10,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost:3306');
 
-define('BASE_PATH', 'http://localhost/framework');
+define('BASE_PATH', 'http://localhost/Ex4.2');
 
 
 define('PAGINATE_LIMIT', '5');
